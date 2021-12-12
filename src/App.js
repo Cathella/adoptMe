010d4876c53faec68900c883d792d96395d9c049
Cathella <ncathie214@gmail.com>
2021-12-12 14:49:03 +0300

    Introducing react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Beam" animal="Dog" breed="Wheaten Terrier" />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/Pet.js
@@ -0,0 +1,11 @@
+const Pet = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h3>{props.animal}</h3>
+      <h3>{props.breed}</h3>
+    </div>
+  )
+}
+
+export default Pet;
